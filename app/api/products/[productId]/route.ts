f1ddc2bae1cf2f9c1967d89c069c50a04150dd84
Abhishek Tyagi <abhishektyagi@gmail.com>
2024-05-26 20:49:04 +0530

    cors origin

--- app/api/products/[productId]/route.ts
+++ app/api/products/[productId]/route.ts
@@ -26,10 +26,11 @@ export const GET = async (
     return new NextResponse(JSON.stringify(product), {
       status: 200,
       headers: {
-        "Access-Control-Allow-Origin": `*`,
-        "Access-Control-Allow-Methods": "GET",
-        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
-      },
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+        "Access-Control-Allow-Credentials": "true"
+      },      
     });
   } catch (err) {
     console.log("[productId_GET]", err);
